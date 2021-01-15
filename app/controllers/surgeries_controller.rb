5c5b38402b17e9ff9cc294a8f1e7f02512e8a900
Max Ribbans <ribbans.max@gmail.com>
2021-01-15 12:08:55 -0500

    Add doctors names to surgeries index page

---
+++ app/controllers/surgeries_controller.rb
@@ -0,0 +1,5 @@
+class SurgeriesController < ApplicationController
+  def index
+    @surgeries = Surgery.all
+  end
+end
--- spec/features/surgeries/index_spec.rb
+++ spec/features/surgeries/index_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe "Surgery Index" do
       expect(page).to have_link(surgery.title, href: surgery_path(surgery))
     end
   end
+
+  it "displays all of a surgery's doctors" do
+    create_list(:surgery, 10)
+    hospital = FactoryBot.create(:hospital)
+
+    Surgery.all.each do |surgery|
+      doctor = FactoryBot.create(:doctor, hospital: hospital)
+      Operation.create(doctor: doctor, surgery: surgery)
+    end
+
+    visit surgeries_path
+
+    Surgery.all.each do |surgery|
+      within("#surgery-#{surgery.id}") do
+        expect(page).to have_content(surgery.doctors.first.name)
+      end
+    end
+  end
 end
